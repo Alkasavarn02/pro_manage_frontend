24a07f488609c81cb589a91abfffdbf2b292f318
Alka savarn <alkasavarn02@gmail.com>
2024-11-30 20:21:38 +0530

    loading state

--- src/Components/ButtonComponent/index.jsx
+++ src/Components/ButtonComponent/index.jsx
@@ -1,11 +1,12 @@
 import styles from "./Button.module.css";
 
-const CustomButton = ({ title, onClick, buttonType, type, classes}) => {
+const CustomButton = ({ title, onClick, buttonType, type, classes, disabled}) => {
     return (  
         <button
             className={`${styles['custom-button']} ${styles[buttonType]} ${classes || ""}`}
             onClick={onClick}
             type={type}
+            disabled={disabled}
         >
             { title }
         </button>
